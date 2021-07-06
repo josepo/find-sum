a1ad7a3bc2dc889a6c0c096ca503aca28e05c3d7
josepo <jlpolo@gmail.com>
2021-07-06 20:26:08 +0200

    Two numbers

--- FindSum/Program.cs
+++ FindSum/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace FindSum
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   {
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         IEnumerable<string> lines = System.IO.File.ReadLines("../input.txt");
+
+         List<int> values =
+            System.IO.File.ReadLines("../input.txt")
+            .Select(line => int.Parse(line))
+            .ToList();
+
+         Console.WriteLine(new Sum().Result(values));
+      }
+   }
 }
