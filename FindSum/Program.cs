62fed8e6cae87c882f3a2d8b77387fb03693d300
josepo <jlpolo@gmail.com>
2021-07-06 17:28:42 +0200

    Create solution and project files

---
+++ FindSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
